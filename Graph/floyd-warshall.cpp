6ce0cbb3549f56a1acef763f47ecc0cbb20926e0
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2025-03-02 19:33:04 +0530

    floyd warshall algo

---
+++ Graph/floyd-warshall.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+  public:
+    void shortestDistance(vector<vector<int>>& mat) {
+        // Code here
+        int n = mat.size();
+        int m = mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == -1){
+                    mat[i][j] = 10000000;  
+                }
+            }
+        } // disturbing the flow 
+        
+        for(int via =0; via < n ; via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    mat[i][j] = min(mat[i][j] , mat[i][via] + mat[via][j]);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 10000000){
+                    mat[i][j] = -1;  
+                }
+            }
+        } 
+        
+        return;
+        
+    }
+};
