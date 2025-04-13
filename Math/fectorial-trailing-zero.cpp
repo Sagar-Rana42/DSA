6cfa04a8dd02a3e437ddce562013c809d739c197
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2025-04-13 18:58:50 +0530

    leetcode -172

---
+++ Math/fectorial-trailing-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int trailingZeroes(int n) {
+            if(n == 0) return 0;
+           int ans = 0;
+    
+           while(n > 0){
+                n /= 5;
+                ans += n;
+           }
+           return ans;
+        }
+    };
